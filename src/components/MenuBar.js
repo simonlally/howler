1e1e9818b222b88b8992530d03b5b634d48fbd5c
Simon Lally <simonlallydev@gmail.com>
2019-08-22 14:49:37 -0400

    updated current path

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,7 +3,9 @@ import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 export default function MenuBar() {
-  const [activeItem, setActiveItem] = useState("home");
+  const pathname = window.location.pathname;
+  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
