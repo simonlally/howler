8da2e30b9607abe876c78e958e2f5a6805d04eed
Simon Lally <simonlallydev@gmail.com>
2019-08-22 14:42:59 -0400

    home, login and register routes

---
+++ src/components/MenuBar.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+export default function MenuBar() {
+  const [activeItem, setActiveItem] = useState("home");
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+  return (
+    <Menu pointing secondary>
+      <Menu.Item
+        name="home"
+        active={activeItem === "home"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
+      <Menu.Item
+        name="register"
+        active={activeItem === "register"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/register"
+      />
+
+      <Menu.Item
+        name="friends"
+        active={activeItem === "friends"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/friends"
+      />
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="login"
+          active={activeItem === "login"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/login"
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+}
