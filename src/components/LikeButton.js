c4797635e088547a3ac10b6b8b527569b9e15c48
Simon Lally <simonlallydev@gmail.com>
2019-08-23 17:57:45 -0400

    styling issues with LikeButton component

---
+++ src/components/LikeButton.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Icon, Label, Button } from "semantic-ui-react";
+
+export default function LikeButton({ user, post: { id, likes } }) {
+  const [liked, setLiked] = useState(false);
+  useEffect(() => {
+    if (user && likes.find(like => like.username === user.username)) {
+      setLiked(true);
+    } else setLiked(false);
+  }, [user, likes]);
+
+  const [likePost] = useMutation(LIKE_POST, {
+    variables: { postId: id }
+  });
+
+  const likeButton = user ? (
+    liked ? (
+      <Button color="blue">
+        <Icon name="heart" />
+      </Button>
+    ) : (
+      <Button color="blue" basic>
+        <Icon name="heart" />
+      </Button>
+    )
+  ) : (
+    <Button as={Link} to="/login" color="blue" basic>
+      <Icon name="heart" />
+    </Button>
+  );
+
+  return (
+    <Button as="div" labelPosition="right" onClick={likePost}>
+      <Label basic color="blue" pointing="left">
+        {likes.length}
+      </Label>
+    </Button>
+  );
+}
+
+const LIKE_POST = gql`
+  mutation likePost($postId: ID!) {
+    likePost(postId: $postId) {
+      id
+      likes {
+        id
+        username
+      }
+    }
+  }
+`;
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
 import moment from "moment";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth";
+import LikeButton from "./LikeButton.js";
 
 export default function PostCard({
   post: { body, createdAt, id, username, likes, comments }
 }) {
+  const { user } = useContext(AuthContext);
   function likePost() {
     console.log("post liked!");
   }
@@ -26,15 +30,18 @@ export default function PostCard({
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Button as="div" labelPosition="right" onClick={likePost}>
-          <Button color="blue">
-            <Icon name="heart" />
-          </Button>
-          <Label as="a" basic color="blue" pointing="left">
-            {likes.length}
-          </Label>
-        </Button>
-        <Button as="div" labelPosition="right" onClick={commentPost}>
+        <LikeButton user={user} post={{ id, likes }} />
+        <Label as="a" basic color="blue" pointing="left">
+          {likes.length}
+        </Label>
+
+        <Button
+          as="div"
+          labelPosition="right"
+          onClick={commentPost}
+          as={Link}
+          to={`/post/${id}`}
+        >
           <Button color="green">
             <Icon name="comment" />
             Comment
@@ -43,6 +50,15 @@ export default function PostCard({
             {comments.length}
           </Label>
         </Button>
+        {user && user.username === username && (
+          <Button
+            as="dev"
+            color="red"
+            onClick={() => console.log("delete post")}
+          >
+            <Icon name="trash" />
+          </Button>
+        )}
       </Card.Content>
     </Card>
   );
