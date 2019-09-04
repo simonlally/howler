38f406bf6bfe79d256df9d384be5180ed77721c3
Simon Lally <simonlallydev@gmail.com>
2019-09-04 09:31:05 -0400

    added callback to return to home posts page

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -3,13 +3,25 @@ import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
 import { Button, Icon, Confirm } from "semantic-ui-react";
 
-export default function DeleteButton({ postId }) {
+import { GET_POSTS_QUERY } from "../util/graphql";
+
+export default function DeleteButton({ postId, callback }) {
   const [confirmOpen, setConfirmOpen] = useState(false);
   const [deletePost] = useMutation(DELETE_POST, {
-    update() {
+    update(proxy) {
       // are you sure you want to delete this post?
       setConfirmOpen(false);
-      // still need to remove post from cache
+
+      const data = proxy.readQuery({
+        query: GET_POSTS_QUERY
+      });
+
+      data.getPosts = data.getPosts.filter(p => p.id !== postId);
+      proxy.writeQuery({ query: GET_POSTS_QUERY, data });
+
+      if (callback) {
+        callback();
+      }
     },
     variables: {
       postId
